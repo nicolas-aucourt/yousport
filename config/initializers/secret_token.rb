05bb5bdcccd34fc56f8a32645d066c91eeaca3f4
nicolas-aucourt <aucourt.nicolas@gmail.com>
2011-06-06 17:54:02 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yousport::Application.config.secret_token = '151bf4b7283b25df3ec40f64ebf5010946733525456d1d26ecd997b9abee5de1dfbe03500559f4d187297e6bbdeaac0558277c40c5c20f8a6bb9efaa1a990544'
