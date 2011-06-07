b07c3c3adb1e2844b1015e5641e2ad9712e675a4
nicolas-aucourt <aucourt.nicolas@gmail.com>
2011-06-07 11:25:59 +0100

    Finished layout and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,30 @@
 class PagesController < ApplicationController
   def home
 	@title="Home"
+	@meta_title="Meta title of the Home"
+	@meta_desc="Meta description of the Home"
+	@h1="YouSport"
   end
 
   def contact
 	@title="Contact"
+	@meta_title="Meta title of the Contact"
+	@meta_desc="Meta description of the Contact"
+	@h1="Contact us"
   end
 
   def about
 	@title="About"
+	@meta_title="Meta title of the About"
+	@meta_desc="Meta description of the About"
+	@h1="About us"
+  end
+
+  def help
+	@title="Help"
+	@meta_title="Meta title of the Help"
+	@meta_desc="Meta description of the Help"
+	@h1="Help"
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,24 @@ module ApplicationHelper
     end
   end
 
+# Return a meta_title on a per-page basis.
+  def meta_title
+    base_meta_title = "- YouSport"
+    if @meta_title.nil?
+      base_meta_title
+    else
+      "#{@meta_title} #{base_meta_title}"
+    end
+  end
+
+# Return a meta_desc on a per-page basis.
+  def meta_desc
+    base_meta_desc = "- YouSport"
+    if @meta_desc.nil?
+      base_meta_desc
+    else
+      "#{@meta_desc} #{base_meta_desc}"
+    end
+  end
+
 end
